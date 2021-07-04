8c94d551d09e9e2339189a2634299aea81d25429
phortuin <phortuin@users.noreply.github.com>
2021-07-04 14:03:02 +0200

    fix: editorconfig, whitespace

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
 require('dotenv-safe').config()
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    }
-  },
+	development: {
+		client: 'pg',
+		connection: process.env.PG_CONNECTION_STRING,
+		migrations: {
+			directory: './data/migrations',
+		},
+		seeds: {
+			directory: './data/seeds',
+		}
+	},
 }
