bc70923c8e26443b86062b6b3e7aa8a6fd49b731
phortuin <phortuin@users.noreply.github.com>
2021-07-04 14:01:55 +0200

    fix: remove duplicate config

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 require('dotenv-safe').config()
 
 module.exports = {
-
   development: {
     client: 'pg',
     connection: process.env.PG_CONNECTION_STRING,
@@ -12,16 +11,4 @@ module.exports = {
       directory: './data/seeds',
     }
   },
-
-  development: {
-    client: 'pg',
-    connection: process.env.PG_CONNECTION_STRING,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    }
-  },
-
-};
+}
