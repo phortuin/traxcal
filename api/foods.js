6cdd5b8d411eea197f93211fb891c32b5fcf4f7c
phortuin <phortuin@users.noreply.github.com>
2021-07-06 20:26:54 +0200

    feat: add delete actions for foods and portions

--- api/foods.js
+++ api/foods.js
@@ -3,7 +3,7 @@ const { handleByMethod, handleErrors } = require('../lib/middleware')
 const knex = require('../lib/knex-instance')
 
 // Endpoint handler
-module.exports = handleErrors(handleByMethod({ post }))
+module.exports = handleErrors(handleByMethod({ post, del }))
 
 /**
  * Save new food in database. Expects a JSON object with properties:
@@ -27,4 +27,19 @@ async function post(req, res) {
 			fat,
 		})
 	res.end(JSON.stringify(response))
+}
+
+/**
+ * Delete food entry from database. Expects an `id` parameter in the body.
+ * Redirects to the site home page after deletion
+ *
+ * @param  {HTTP Request Object} req
+ * @param  {HTTP Response Object} res
+ */
+async function del(req, res) {
+	await knex('foods')
+		.where('id', req.body.id)
+		.del()
+	res.writeHead(303, { location: '/' })
+	res.end()
 }
--- api/portions.js
+++ api/portions.js
@@ -3,7 +3,7 @@ const { handleByMethod, handleErrors } = require('../lib/middleware')
 const knex = require('../lib/knex-instance')
 
 // Endpoint handler
-module.exports = handleErrors(handleByMethod({ post }))
+module.exports = handleErrors(handleByMethod({ post, del }))
 
 /**
  * Save new portion in database. Expects a JSON object with properties:
@@ -23,4 +23,19 @@ async function post(req, res) {
 			food_id
 		})
 	res.end(JSON.stringify(response))
+}
+
+/**
+ * Delete portion entry from database. Expects an `id` parameter in the body.
+ * Redirects to the site home page after deletion
+ *
+ * @param  {HTTP Request Object} req
+ * @param  {HTTP Response Object} res
+ */
+async function del(req, res) {
+	await knex('portions')
+		.where('id', req.body.id)
+		.del()
+	res.writeHead(303, { location: '/' })
+	res.end()
 }
